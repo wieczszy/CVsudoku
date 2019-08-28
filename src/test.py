f7f09642bfbd7acc35e584f591c268f9aa55a473
fivtyen <5509131+fivtyen@users.noreply.github.com>
2019-08-28 21:35:59 +0200

    add solver to the test

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 import numpy as np
-from reader import ImageReader
 from classfier import Classifier
+from reader import ImageReader
+from solver import SudokuSolver
 
 
 if __name__ == '__main__':
@@ -10,6 +11,10 @@ if __name__ == '__main__':
     cells = reader.extract_board_cells(file_path)
     cells = cells.reshape(-1,28, 28, 1)
 
-    c = Classifier('model.h5')
-    classifications = np.array(c.classify_cells(cells)).reshape((9,9))
-    print(classifications)
+    classifier = Classifier('model.h5')
+    classifications = classifier.classify_cells(cells)
+    classifications = [str(c) for c in classifications]
+    grid = ''.join(classifications)
+
+    solver = SudokuSolver()
+    solver.solve(grid)
